90efb52305b5c2e7c5b75bf7e1c718095c105f4a
tanjim hasan <tanjim13hasan@gmail.com>
2024-11-15 17:45:19 +0600

    Added Hero and Navbar and blogStyles.css on Blog pag

--- my-app/src/routes/Blog.jsx
+++ my-app/src/routes/Blog.jsx
@@ -1,7 +1,13 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import BlogImg from "../assets/7.BlogImg.png";
+import "./blogStyles.css";
+
 const Blog = () => {
   return (
     <>
-      <h1>I'm Blog page</h1>
+      <Navbar />
+      <Hero cName="hero-blog" heroImg={BlogImg} title="Blog" />
     </>
   );
 };
